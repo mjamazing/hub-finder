a345e279c3c9286f2012fb904eb4932f92f40401
Jason Grace <jason@thegracefamily.net>
2017-06-03 23:49:11 -0400

    Formatting addresses

--- js/application/hubfinder.js
+++ js/application/hubfinder.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 
     $("#findhubsbutton").click(function() {
-
+        var hubs = $("#hublist").val().split("\n");
+        var addresses = $("#addresslist").val().split("\n");
+        hubs = $.map(hubs, function(val) {
+            return val.replace(/ /g, "+").replace(/,/g, "+");
+        });
+        addresses = $.map(addresses, function(val) {
+            return val.replace(/ /g, "+").replace(/,/g, "+");
+        });
+        var hublist = hubs.join("|");
+        var addresslist = addresses.join("|");
+        alert(hublist);
+        alert(addresslist);
     });
 
 });
