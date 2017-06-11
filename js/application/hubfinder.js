8fa93dbb0737dbb1275b1c1ea383d792bb6cde19
Jason Grace <jason@thegracefamily.net>
2017-06-10 22:17:46 -0400

    List closest hubs and display results

--- js/application/hubfinder.js
+++ js/application/hubfinder.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    google.load('maps', '3.27', { other_params: 'key=API_KEY', callback: init });
+
+    function init() {}
 
     $("#findhubsbutton").click(function() {
         var hubs = $("#hublist").val().split("\n");
@@ -6,8 +9,8 @@ $(document).ready(function() {
 
         var service = new google.maps.DistanceMatrixService();
         service.getDistanceMatrix({
-            origins: hubs,
-            destinations: addresses,
+            origins: addresses,
+            destinations: hubs,
             travelMode: google.maps.TravelMode.DRIVING,
             unitSystem: google.maps.UnitSystem.IMPERIAL,
             avoidHighways: false,
@@ -19,19 +22,28 @@ $(document).ready(function() {
         if (status != google.maps.DistanceMatrixStatus.OK) {
             $('#results').html("A parsing error has occurred.");
         } else {
-            $("#results").text(JSON.stringify(response));
-            // var origin = response.originAddresses[0];
-            // var destination = response.destinationAddresses[0];
-            // if (response.rows[0].elements[0].status === "ZERO_RESULTS") {
-            //     $('#result').html("Better get on a plane. There are no roads between " +
-            //         origin + " and " + destination);
-            // } else {
-            //     var distance = response.rows[0].elements[0].distance;
-            //     var distance_value = distance.value;
-            //     var distance_text = distance.text;
-            //     var miles = distance_text.substring(0, distance_text.length - 3);
-            //     $('#result').html("It is " + miles + " miles from " + origin + " to " + destination);
-            // }
+            for (i = 0; i < response.originAddresses.length; i++) {
+                originAddress = '';
+                destinationAddresses = [];
+                // distance = 0;
+                originAddress = response.originAddresses[i];
+                $("#results").append("<div class='hubresult'><b>" + originAddress + "</b>");
+                // Switch to rows/elements
+                for (j = 0; j < response.destinationAddresses.length; j++) {
+                    destinationAddresses[j] = [];
+                    destinationAddresses[j]['address'] = response.destinationAddresses[j];
+                    destinationAddresses[j]['distance_text'] = response.rows[i].elements[j].distance.text;
+                    destinationAddresses[j]['distance_value'] = response.rows[i].elements[j].distance.value;
+                }
+                destinationAddresses.sort(function(a, b) {
+                    return a['distance_value'] - b['distance_value'];
+                });
+                for (j = 0; j < destinationAddresses.length; j++) {
+                    $("#results").append("<div class='hubresult'>" + destinationAddresses[j]['address'] + " " + destinationAddresses[j]['distance_text'] + "</div>");
+                    // $("#results").append(address, " -&gt; ", hubAddress, "<br/>");
+                }
+                $("#results").append("</div><br/>");
+            }
         }
     }
 });
