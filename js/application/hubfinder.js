564915d3cf117809118003095421be48b99c2fe2
Jason Grace <jason@thegracefamily.net>
2017-06-12 22:29:28 -0400

    Moved map to right

--- js/application/hubfinder.js
+++ js/application/hubfinder.js
@@ -26,18 +26,10 @@ $(document).ready(function() {
             for (i = 0; i < response.originAddresses.length; i++) {
                 originAddress = '';
                 destinationAddresses = [];
+                mapID = 'map_' + i;
                 // distance = 0;
                 originAddress = response.originAddresses[i];
                 $("#results").append("<div class='hubresult'><div id='hubaddress_" + i + "' style='font-weight: bold;'>" + originAddress + "</div>");
-                mapID = "map_" + i;
-                $("#results").append("<div class='map' id='" + mapID + "' style='width: 300px; height: 300px;'></div>");
-                maps[i] = new google.maps.Map(document.getElementById(mapID), {
-                    center: { lat: 0.0, lng: 0.0 },
-                    zoom: 10
-                });
-                var bounds = new google.maps.LatLngBounds();
-
-                marker = addMarker(maps[i], originAddress, bounds);
 
                 for (j = 0; j < response.destinationAddresses.length; j++) {
                     destinationAddresses[j] = [];
@@ -53,12 +45,33 @@ $(document).ready(function() {
                 destinationAddressTableOutput += "<thead><tr><th>Hub #</th><th>Hub</th><th>Distance</th></tr></thead>";
                 destinationAddressTableOutput += "<tbody>";
                 for (j = 0; j < destinationAddresses.length; j++) {
-                    marker = addMarker(maps[i], destinationAddresses[j]['address'], bounds, (j + 1).toString());
                     destinationAddressTableOutput += "<tr><td>" + (j + 1) + "</td><td>" + destinationAddresses[j]['address'] + "</td><td>" + destinationAddresses[j]['distance_text'] + "</td></tr>";
                 }
-                destinationAddressTableOutput += "</tbody></table><br/>";
+                destinationAddressTableOutput += "</tbody></table>";
                 $("#results").append(destinationAddressTableOutput);
+                $("#results").append("<span class='map' id='" + mapID + "'><input class='drawmapbutton' id='drawmap_" + i + "' type='button' value='Show Map'/></span>");
+                $("#drawmap_" + i).click({ mapID: mapID, originAddress: originAddress, destinationAddresses: response.destinationAddresses }, function(event) {
+                    drawMap(event.data.mapID, event.data.originAddress, event.data.destinationAddresses);
+                });
             }
+        }
+    }
+
+    function drawMap(mapID, originAddress, destinationAddresses) {
+        // mapID = "map_" + element;
+        mapElement = document.getElementById(mapID);
+        // $("#" + mapID).css("width", 300);
+        // $("#" + mapID).css("height", 300);
+        // ("<div class='map' id='" + mapID + "' style='width: 300px; height: 300px;'></div>");
+        var map = new google.maps.Map(mapElement, {
+            center: { lat: 0.0, lng: 0.0 },
+            zoom: 10
+        });
+        var bounds = new google.maps.LatLngBounds();
+
+        marker = addMarker(map, originAddress, bounds);
+        for (j = 0; j < destinationAddresses.length; j++) {
+            marker = addMarker(map, destinationAddresses[j], bounds, (j + 1).toString());
         }
     }
 
